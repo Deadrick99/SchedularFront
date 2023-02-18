edeab1f4ffa11be12236b08309ed8d9328d0d316
William <33364879+Deadrick99@users.noreply.github.com>
2023-02-18 00:11:03 -0500

    schedulaer

--- Schedular/src/main.jsx
+++ Schedular/src/main.jsx
@@ -1,16 +1,17 @@
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter,Routes,  Route } from 'react-router-dom'
-import App from './App'
-import './App.css'
-import { AuthProvider } from './Context/AuthProvider'
-ReactDOM.createRoot(document.getElementById('root')).render(
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+import "./App.css";
+import React from "react";
+import { AuthProvider } from "./Context/AuthProvider";
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-    <AuthProvider>
-    <Routes>
-      <Route path='/*'element= {<App/>}/>
-    </Routes>
-    </AuthProvider>
+      <AuthProvider>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
