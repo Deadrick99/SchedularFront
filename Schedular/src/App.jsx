11fa78e3fc983dbe5d41f5caec80400c2585ccb7
William <33364879+Deadrick99@users.noreply.github.com>
2023-02-17 23:46:04 -0500

    removed unused imports

--- Schedular/src/App.jsx
+++ Schedular/src/App.jsx
@@ -1,10 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+
 import './App.css'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import { Routes,Route } from 'react-router'
-import "../dist/output.css"
 import Login from './components/Login'
 import Register from './components/Register'
 import Layout from './components/Layout'
